528245185da3483dc69c335b637862fcc917c506
fxxwol <protsivroksolana2004@gmail.com>
2023-07-11 02:32:38 +0300

    move logic to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,70 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import SearchBar from './SearchBar';
 import ImageGallery from './ImageGallery';
 import Modal from './Modal';
+import { fetchImg } from 'service/fetchImg';
+import Loader from './Loader';
+import Button from './Button';
 
 export default class App extends Component {
   state = {
+    gallery: [],
+    page: 1,
+    totalPages: 1,
+    error: null,
+    status: 'idle',
     query: '',
     showModal: false,
-    selectedImg: {}
+    selectedImg: {},
   };
+
+  componentDidUpdate(_, prevState) {
+    const prevQuery = prevState.query;
+    const currentQuery = this.state.query;
+
+    if (prevQuery !== currentQuery) {
+      this.setState({ status: 'pending', page: 1 });
+      this.getData(1);
+    }
+
+    if (prevState.gallery !== this.state.gallery) {
+      this.isLoading = true;
+    }
+  }
+
+  getData = async page => {
+    try {
+      const data = await fetchImg(this.state.query, page);
+      if (!data.total) {
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      this.setState({
+        status: 'resolved',
+        gallery: data.hits,
+        totalPages: Math.ceil(data.totalHits / 12),
+      });
+    } catch (error) {
+      this.setState({ error, status: 'rejected' });
+    }
+  };
+
+  loadMore = async () => {
+    const currentPage = this.state.page + 1;
+    try {
+      const data = await fetchImg(this.state.query, currentPage);
+      if (currentPage > this.state.totalPages) {
+        return;
+      }
+      this.setState(prev => ({
+        page: currentPage,
+        gallery: prev.gallery.concat(data.hits),
+      }));
+    } catch (error) {
+      this.setState({ error, status: 'rejected' });
+    }
+  };
+
   handleSearch = ({ query }) => {
     this.setState({ query });
   };
@@ -23,14 +80,24 @@ export default class App extends Component {
   };
 
   render() {
+    const {status, page, totalPages, error} = this.state
     return (
       <div className="App">
         <SearchBar onSubmit={this.handleSearch} />
-        <ImageGallery
-          query={this.state.query}
-          onModalClick={this.toggleModal}
-        />
-        {this.state.showModal && <Modal img={ this.state.selectedImg} toggleModal={this.toggleModal} />}
+        {status === 'pending' && <Loader />}
+        {status === 'resolved' && (
+          <>
+            <ImageGallery
+              gallery={this.state.gallery}
+              onModalClick={this.toggleModal}
+            />
+            {!(page === totalPages) && <Button onClick={this.loadMore} />}
+          </>
+        )}
+        {status === 'rejected' && <h1>{error.message}</h1>}
+        {this.state.showModal && (
+          <Modal img={this.state.selectedImg} toggleModal={this.toggleModal} />
+        )}
         <ToastContainer autoClose={80000} theme="colored" />
       </div>
     );
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,97 +1,29 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { fetchImg } from 'service/fetchImg';
-import Loader from '../components/Loader';
-import ImageGalleryItem from './ImageGalleryItem';
-import Button from './Button';
+import React from 'react'
+import PropTypes from 'prop-types'
+import ImageGalleryItem from './ImageGalleryItem'
 
-export default class ImageGallery extends Component {
-  static propTypes = {
-    query: PropTypes.string.isRequired,
-    onModalClick: PropTypes.func.isRequired,
-  };
-
-  state = {
-    gallery: [],
-    page: 1,
-    totalPages: 1,
-    error: null,
-    status: 'idle',
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevProps.query;
-    const currentQuery = this.props.query;
-
-    if (prevQuery !== currentQuery) {
-      this.setState({ status: 'pending', page: 1 });
-      this.getData(1);
-    }
-
-    if (prevState.gallery !== this.state.gallery) {
-      this.isLoading = true;
-    }
-  }
-
-  getData = async page => {
-    try {
-      const data = await fetchImg(this.props.query, page);
-      if (!data.total) {
-        throw new Error(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      this.setState({
-        status: 'resolved',
-        gallery: data.hits,
-        totalPages: Math.ceil(data.totalHits / 12),
-      });
-    } catch (error) {
-      this.setState({ error, status: 'rejected' });
-    }
-  };
-
-  loadMore = async () => {
-    const currentPage = this.state.page + 1;
-    try {
-      const data = await fetchImg(this.props.query, currentPage);
-      if (currentPage > this.state.totalPages) {
-        return;
-      }
-      this.setState(prev => ({
-        page: currentPage,
-        gallery: prev.gallery.concat(data.hits),
-      }));
-    } catch (error) {
-      this.setState({ error, status: 'rejected' });
-    }
-  };
-
-  render() {
-    const { status, gallery, page, totalPages, error } = this.state;
-    if (status === 'pending') {
-      return <Loader />;
-    }
-    if (status === 'resolved') {
-      return (
-        <>
-          <ul className="ImageGallery">
+const ImageGallery = ({gallery, onModalClick}) => {
+  return (
+    <div>
+      <ul className="ImageGallery">
             {gallery.map(item => {
               return (
                 <ImageGalleryItem
                   item={item}
-                  onModalClick={this.props.onModalClick}
+                  onModalClick={onModalClick}
                   key={item.id}
                 />
               );
             })}
           </ul>
-          {!(page === totalPages) && <Button onClick={this.loadMore} />}
-        </>
-      );
-    }
-    if (status === 'rejected') {
-      return <h1>{error.message}</h1>;
-    }
-  }
+    </div>
+  )
 }
+
+ImageGallery.propTypes = {
+  gallery: PropTypes.arrayOf(PropTypes.object,),
+  onModalClick: PropTypes.func,
+}
+
+export default ImageGallery
+
